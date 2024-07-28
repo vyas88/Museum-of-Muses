2be13b1a6966c724b8019eb2fd8718e503893d86
Ronit Vyas <122534713+vyas88@users.noreply.github.com>
2024-07-28 21:04:47 +0530

    Add files via upload

---
+++ SEM1_WEbTech-project/navigation menu.js
@@ -0,0 +1,11 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+	window.alert('under maintenance.... ;)')
+  
+});
+
+
